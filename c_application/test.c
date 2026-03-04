3ed893d0590c3cebeb862313c94a806344bc8eda
yeeun.flow <yeeun.flow@gmail.com>
2026-03-04 10:26:02 +0900

    논리 연산 PUSH 테스트
    비트 연산 예제 코드로 main.c 전체 교체
    
    기존 논리 연산 예제를 삭제하고, 비트 연산(&, |, ^, ~) 결과를 16진수와 함께 출력하는## 코드로 main.c를 전면 수정했습니다. main 함수 선언도 main(void)로 명확히 변경했습니다.

--- c_application/test.c
+++ c_application/test.c
@@ -4,7 +4,7 @@ int main(void)
 {
 	int x = 15; // 1111
 
-	printf("%3d %08x\n", x, x);		 // 1111
+	printf("test\n", x, x);		 // 1111
 	printf("%3d %08x\n", 1, x & 1);  // 1111 & 0001
 	printf("%3d %08x\n", 15, x | 1); // 1111 | 0001
 	printf("%3d %08x\n", 14, x ^ 1); // 1111 ^ 0001
