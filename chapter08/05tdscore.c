f2633cbee336679177b27541faa8d350ff4e911e
yeeun-flow <yeeun.flow@gmail.com>
2026-03-31 01:07:11 +0900

    chapter08 stub으로 초기화 - classroom assignment 배포용

--- chapter08/05tdscore.c
+++ chapter08/05tdscore.c
@@ -1,31 +1,21 @@
 // file: tdscore.c
 #include <stdio.h>
-#define ROWSIZE 4
-#define COLSIZE 2
+
+// ROWSIZE라는 이름으로 정수 4를, COLSIZE라는 이름으로 정수 2를 나타내는 매크로 상수를 정의하세요
 
 int main(void)
 {
-	int sum = 0, midsum = 0, finalsum = 0;
+	// 합계, 중간 합계, 기말 합계를 저장할 int형 변수 sum, midsum, finalsum을 0으로 초기화하세요
 
 	// 2차원 배열 초기화
-	int score[][COLSIZE] = { 95, 85, 90, 88, 86, 90, 88, 78 };
+	// 열 크기가 COLSIZE인 int형 2차원 배열 score를 { 95, 85, 90, 88, 86, 90, 88, 78 }로 초기화하세요
 
 	printf("       중간       기말\n");
 	printf("    --------------------\n");
-	for (int i = 0; i < ROWSIZE; i++)
-	{
-		for (int j = 0; j < COLSIZE; j++)
-		{
-			printf("%10d ", score[i][j]);
-			sum += score[i][j];
-			if (j == 0) 
-				midsum += score[i][j];
-			else 
-				finalsum += score[i][j];
-		}
-		puts("");
-	}
-
+	// ROWSIZE x COLSIZE를 순회하는 중첩 for문을 작성하세요
+	// 각 원소를 printf("%10d ", score[i][j]); 형식으로 출력하고 sum에 누적하세요
+	// j == 0이면 midsum에, 아니면 finalsum에 누적하세요
+	// 각 행 출력 후 puts(""); 으로 줄바꿈하세요
 	printf("    --------------------\n");
 	printf("평균: %6.2f %10.2f\n", (double)midsum / ROWSIZE, (double)finalsum / ROWSIZE);
 	printf("\n성적의 합은 %d이고 ", sum);
--- chapter08/lab1inputarray.c
+++ chapter08/lab1inputarray.c
@@ -4,19 +4,15 @@
 int main(void)
 {
 	//초기화로 모든 원소에 0을 저장
-	int input[20] = { 0 };
+	// 크기가 20인 int형 배열 input을 선언하고 모든 원소를 0으로 초기화하세요
 
 	printf("배열에 저장할 정수를 여러 개 입력하시오. ");
 	printf("0을 입력하면 입력을 종료합니다.\n");
-	int i = 0;
-	do {
-		scanf("%d", &input[i]);
-	} while (input[i++] != 0);
+	// 인덱스 변수 i를 0으로 초기화하세요
+	// do-while문을 사용해 scanf("%d", &input[i]); 로 정수를 입력받고, input[i++] != 0인 동안 반복하세요
 
-	i = 0;
-	while (input[i] != 0) {
-		printf("%d ", input[i++]);
-	}
+	// i를 0으로 초기화하세요
+	// input[i]가 0이 아닌 동안 printf("%d ", input[i++]); 형식으로 출력하는 while문을 작성하세요
 	puts("");
 
 	return 0;
