34b98a7918525a1de7630c85b67f32d947b953bf
yeeun-flow <yeeun.flow@gmail.com>
2026-05-27 01:30:33 +0900

    Adjust chapter 14 vararg stub

--- chapter14/03vararg.c
+++ chapter14/03vararg.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h> //가변인자를 위한 헤더 파일
 
-double avg(int, ...); //int 이후는 가변인자 ...
+double avg(        ); // 가이드: 첫 매개변수 뒤에 가변인자 ... 선언
 
 int main(void)
 {
@@ -12,16 +12,19 @@ int main(void)
 }
 
 //가변인자 ... 시작 전 첫 고정 매개변수는 가변인자를 처리하는데 필요한 정보를 지정
-double avg(int numagrs, ...) //매개변수 numargs는 가변인자의 수를 지정 
+double avg(        ) // 가이드: int numagrs, ... 
 {
 	double total = 0; //합이 저장될 변수
 
-	va_list argp; //1. 가변인자 변수 선언
-	va_start(argp,        ); //2. numargs 이후의 가변인자 처리 시작 
+	// 1. 가변인자 변수 선언
+	
+	// 2. numargs 이후의 가변인자 처리 시작
+	
 	for (int i = 0; i < numagrs; i++) //3. 가변인자 얻기
 		// 가이드: double 형 가변인자를 하나씩 꺼내 total에 더하기
 		total += ;
-	va_end(argp); //4. 가변인자 처리 종료
+	// 4. 가변인자 처리 종료
+	
 
 	return ;
 }
