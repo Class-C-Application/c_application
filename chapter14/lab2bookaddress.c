427ef298d0ca170064bbefc94dbbe17efd8d06e4
yeeun-flow <yeeun.flow@gmail.com>
2026-05-27 01:37:10 +0900

    Adjust chapter 14 lab stubs

--- chapter14/lab2bookaddress.c
+++ chapter14/lab2bookaddress.c
@@ -9,25 +9,25 @@ typedef struct book
 	int   ISBN;
 } book;
 
-void print(book* b);
+void print(        ); // 가이드: book 구조체 포인터를 매개변수로 선언
 
 int main()
 {
 	book python = { "파이썬으로 배우는 누구나 코딩", "강환수", 979117 };
 	book comintro;
 	// 가이드: strcpy로 title, author를 저장하고 ISBN을 대입
-	strcpy(        , "소프트웨어 중심사회의 컴퓨터개론");
-	strcpy(        , "강환수");
+	strcpy(        , "소프트웨어 중심사회의 컴퓨터개론"); // 가이드: comintro의 title 멤버
+	strcpy(        , "강환수"); // 가이드: comintro의 author 멤버
 	comintro.ISBN = ;
-	print(        );
-	print(        );
+	print(        ); // 가이드: comintro의 주소 전달
+	print(        ); // 가이드: python의 주소 전달
 
 	return 0;
 }
 
-void print(book* b)
+void print(        ) // 가이드: book* b
 {
-	printf("제목: %s, ",        );
-	printf("저자: %s, ",        );
-	printf("ISBN: %d\n",        );
+	printf("제목: %s, ",        ); // 가이드: 구조체 포인터 b로 title 멤버 접근
+	printf("저자: %s, ",        ); // 가이드: 구조체 포인터 b로 author 멤버 접근
+	printf("ISBN: %d\n",        ); // 가이드: 구조체 포인터 b로 ISBN 멤버 접근
 }
--- chapter14/lab3ptrfunction.c
+++ chapter14/lab3ptrfunction.c
@@ -6,22 +6,23 @@ int subt(int a, int b);
 
 int main(void)
 {
-	int (*pfunary[3])(int, int);
-	pfunary[0] = ;
-	pfunary[1] = ;
-	pfunary[2] = ;
+	// 가이드: int 2개를 매개변수로 받고 int를 반환하는 함수 포인터 배열 선언
+	int (*pfunary[3])(        );
+	pfunary[0] = ; // 가이드: add 함수 저장
+	pfunary[1] = ; // 가이드: mult 함수 저장
+	pfunary[2] = ; // 가이드: subt 함수 저장
 
 	int m = 8, n = 6;
 	char* ops = "*+-";
 	char op;
-	while (op = *ops++)
+	while (        ) // 가이드: ops가 가리키는 문자를 op에 저장한 뒤 ops를 다음 문자로 이동
 		switch (op)
 		{
-			case '+': printf("%c 결과: %d\n", op, pfunary[   ](m, n));
+			case '+': printf("%c 결과: %d\n", op, pfunary[   ](m, n)); // 가이드: add가 저장된 인덱스
 				break;
-			case '-': printf("%c 결과: %d\n", op, pfunary[   ](m, n));
+			case '-': printf("%c 결과: %d\n", op, pfunary[   ](m, n)); // 가이드: subt가 저장된 인덱스
 				break;
-			case '*': printf("%c 결과: %d\n", op, pfunary[   ](m, n));
+			case '*': printf("%c 결과: %d\n", op, pfunary[   ](m, n)); // 가이드: mult가 저장된 인덱스
 				break;
 		}
 
@@ -30,13 +31,13 @@ int main(void)
 
 int add(int a, int b)
 {
-	return ;
+	return ; // 가이드: a와 b의 합
 }
 int mult(int a, int b)
 {
-	return ;
+	return ; // 가이드: a와 b의 곱
 }
 int subt(int a, int b)
 {
-	return ;
+	return ; // 가이드: a와 b의 차
 }
