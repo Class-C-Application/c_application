8d505e6154ee3616457342d9257f61533b86b2ac
yeeun-flow <yeeun.flow@gmail.com>
2026-05-27 01:34:19 +0900

    Adjust chapter 14 function array stub

--- chapter14/09aryptrf.c
+++ chapter14/09aryptrf.c
@@ -1,16 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void add(double*, double, double);
-void subtract(double*, double, double);
-void multiply(double*, double, double);
-void devide(double*, double, double);
+void add(        , double, double); // 가이드: 첫 매개변수는 결과를 저장할 double 포인터
+void subtract(        , double, double);
+void multiply(        , double, double);
+void devide(        , double, double);
 
 int main(void)
 {
 	char op[4] = { '+', '-', '*', '/' };
 	//함수 포인터 선언하면서 초기화 과정 
-	void (*fpary[4])(double*, double, double) = {        ,        ,        ,         };
+	// 가이드: 반환값이 없고, double 포인터와 double 2개를 매개변수로 받는 함수 포인터 배열 선언
+	void (*fpary[4])(        , double, double) = {        ,        ,        ,         };
 
 	double m, n, result;
 	printf("사칙연산을 수행할 실수 2개를 입력하세요. >> ");
@@ -18,7 +19,7 @@ int main(void)
 	//사칙연산을 배열의 첨자를 이용하여 수행
 	for (int i = 0; i < 4; i++)
 	{
-		fpary[i](        , m, n);
+		fpary[i](        , m, n); // 가이드: 첫 인자로 result의 주소 전달
 		printf("%.2lf %c %.2lf == %.2lf\n", m, op[i], n, result);
 	}
 
@@ -26,22 +27,22 @@ int main(void)
 }
 
 // x + y 연산 결과를 z가 가리키는 변수에 저장하는 함수 
-void add(double* z, double x, double y)
+void add(        , double x, double y) // 가이드: 첫 매개변수는 double* z
 {
 	
 }
 // x - y 연산 결과를 z가 가리키는 변수에 저장하는 함수 
-void subtract(double* z, double x, double y)
+void subtract(        , double x, double y) // 가이드: 첫 매개변수는 double* z
 {
 	
 }
 // x * y 연산 결과를 z가 가리키는 변수에 저장하는 함수 
-void multiply(double* z, double x, double y)
+void multiply(        , double x, double y) // 가이드: 첫 매개변수는 double* z
 {
 	
 }
 // x / y 연산 결과를 z가 가리키는 변수에 저장하는 함수 
-void devide(double* z, double x, double y)
+void devide(        , double x, double y) // 가이드: 첫 매개변수는 double* z
 {
 	
 }
