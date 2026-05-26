7a0bd0d28e4c6ea24639d2ec561ba5b3de1a6bae
yeeun-flow <yeeun.flow@gmail.com>
2026-05-27 01:33:36 +0900

    Adjust chapter 14 ptrfunction stub

--- chapter14/08ptrfunction.c
+++ chapter14/08ptrfunction.c
@@ -1,24 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void add(double*, double, double);
-void subtract(double*, double, double);
+void add(        , double, double); // 가이드: 첫 매개변수는 결과를 저장할 double 포인터
+void subtract(        , double, double); // 가이드: 첫 매개변수는 결과를 저장할 double 포인터
 
 int main(void)
 {
-	void (*pf)(double*, double, double) = NULL; //함수 포인터 pf를 선언
+	// 가이드: double 포인터, double, double을 매개변수로 받고 반환값이 없는 함수 포인터 pf 선언
+	void (*pf)(        , double, double) = NULL;
 
 	double m, n, result = 0;
 	printf("연산 +, -를 수행할 실수 2개를 입력하세요. >> ");
 	scanf("%lf %lf", &m, &n);
 
 	pf = ; //add() 함수를 함수 포인터 pf에 저장 
-	pf(        , m, n);  //add(&result, m, n);
+	pf(        , m, n);  //가이드: 첫 인자로 result의 주소 전달
 	printf("\n더하기 수행: %lf + %lf == %lf\n", m, n, result);
 	printf("%p %p\n", pf, add);
 
 	pf = ; //subtract() 함수를 함수 포인터 pf에 저장
-	pf(        , m, n);  //subtract(&result, m, n);
+	pf(        , m, n);  //가이드: 첫 인자로 result의 주소 전달
 	printf("  빼기 수행: %lf - %lf == %lf\n", m, n, result);
 	printf("%p %p\n", pf, subtract);
 
@@ -26,13 +27,13 @@ int main(void)
 }
 
 // x + y 연산 결과를 z가 가리키는 변수에 저장하는 함수 
-void add(double* z, double x, double y)
+void add(        , double x, double y) // 가이드: 첫 매개변수는 double* z
 {
 	// 가이드: z가 가리키는 변수에 x + y 저장
 	
 }
 // x - y 연산 결과를 z가 가리키는 변수에 저장하는 함수 
-void subtract(double* z, double x, double y)
+void subtract(        , double x, double y) // 가이드: 첫 매개변수는 double* z
 {
 	// 가이드: z가 가리키는 변수에 x - y 저장
 	
