d247ec800b98ffe13127b9a302ec74f30b6351c3
yeeun-flow <yeeun.flow@gmail.com>
2026-06-10 01:22:21 +0900

    Update prj06-linkedlist.c

--- chapter16/prj06-linkedlist.c
+++ chapter16/prj06-linkedlist.c
@@ -1,5 +1,9 @@
 #include "prj06-linkedlist.h"
 
+LINK createNode(char* name);
+LINK append(LINK head, LINK cur);
+int printList(LINK head);
+
 int main(void)
 {
     char name[30];
@@ -7,20 +11,15 @@ int main(void)
     LINK cur;
 
     printf("이름을 입력하고 Enter를 누르세요. >> \n");
-
-    // 가이드: EOF가 입력될 때까지 이름을 입력받아 노드를 만들고 리스트 뒤에 추가하세요.
-    while (fgets(name, sizeof(name), stdin) != NULL)
+    while (gets(name) != NULL)
     {
-        name[strcspn(name, "\n")] = '\0';
-
-        cur = [          ](name);
+        cur = createNode(name);
         if (cur == NULL) {
             printf("동적메모리 할당에 문제가 있습니다.\n");
             exit(1);
         }
-
-        head = [      ](head, cur);
-        [        ](head);
+        head = append(head, cur);
+        printList(head);
     }
 
     return 0;
